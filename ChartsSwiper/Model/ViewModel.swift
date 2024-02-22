45556cfcf8c9f8b82d3645420af8f9e2c6b963e2
Jakob Ossmann <Jakobossmann@gmail.com>
2024-02-22 11:31:10 +1000

    New ViewModel and Datastructure - Merge API and CoreData and create DetailStock info

--- ChartsSwiper/Model/ViewModel.swift
+++ ChartsSwiper/Model/ViewModel.swift
@@ -1,32 +1,23 @@
 import SwiftUI
+import Foundation
 import CoreData
+import Combine
 
 struct YahooFinanceResponse: Codable {
-    struct MetaData: Codable {
-        let symbol: String
+    struct Meta: Codable {
         let regularMarketPrice: Double
     }
-            
-    struct Item: Codable {
+    let meta: Meta
+    let body: [String: HistoryItem]
+    
+    struct HistoryItem: Codable {
         let date: String
-        let date_utc: Int
-//        let open: Double
-//        let high: Double
-//        let low: Double
         let close: Double
         let volume: Int
     }
-    
-    let meta: MetaData
-    let body: [String: Item]
-
-    enum CodingKeys: String, CodingKey {
-        case meta
-        case body
-    }
 }
 
-struct Stock: Identifiable, Codable {
+struct APIStock: Identifiable, Codable {
     let id = UUID()
     let symbol: String
     let regularMarketPrice: Double
@@ -43,108 +34,184 @@ struct StockHistory: Codable {
     let volume: Int
 }
 
-// View Model
-class APIStockViewModel: ObservableObject {
-    @Published var stocks: [Stock] = []
+// The main stock entity where every data we need is combined (Live API, matchScore, Indicators from Yahoo Finance CSV, historical data)
+struct DetailStock: Identifiable, Codable {
+    let id = UUID()
+    let symbol: String
+    let regularMarketPrice: Double
+    let matchScore: Float
+    let peRatio: Float
+    let history: [StockHistory] // Array to hold all the close prices
+}
 
-    func fetchAPIKey() -> String? {
-        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
-              let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {
-            return nil
-        }
-        return dict["APIKey"] as? String
-    }
-
-    func fetchData() {
-        guard let apiKey = fetchAPIKey() else {
-            print("API Key not found")
-            return
-        }
-        // Use apiKey here
-        print("API Key: \(apiKey)")
-    
+// Update Core Data with PE Ratios and MatchScores
+class MatchService: ObservableObject {
+    func updateMatchScores(peRatioPreference: Float) {
+        let viewContext = PersistenceController.shared.container.viewContext
         
-        // List of stock symbols
-        let largeCaps = [
-            "AAPL", "META", "GM"
-        ]
+        let fetchRequest: NSFetchRequest<DBStock> = DBStock.fetchRequest()
         
-        // Create a URLSession for making API requests
-        let session = URLSession.shared
-        
-        print("Console Check 2")
-        // Loop through each stock symbol and fetch data
-        for symbol in largeCaps {
-            // Define the API URL for the current stock symbol
-            let urlString = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=\(symbol)&interval=1d&diffandsplits=false"
+        do {
+            let stocks = try viewContext.fetch(fetchRequest)
             
-            print("Console Check 3")
-            // Create a URL object
-            if let url = URL(string: urlString) {
-                // Create a URLRequest with headers
-                var request = URLRequest(url: url)
-                request.httpMethod = "GET"
-                request.setValue(apiKey, forHTTPHeaderField: "X-RapidAPI-Key")
-                request.setValue("yahoo-finance15.p.rapidapi.com", forHTTPHeaderField: "X-RapidAPI-Host")
+            print("test match calculation")
+            
+            for stock in stocks {
+                    stock.matchScore = peRatioPreference * (stock.peRatio ?? 0)
                 
-                print("Console Check 4")
-                // Create a URLSession task to fetch the data
-                session.dataTask(with: request) { data, response, error in
-
-                    
-                    print("Console Check 5")
-                    if let data = data {
-                        do {
-                            // Parse the JSON data
-                            let decoder = JSONDecoder()
-                            var stockHistory: [StockHistory] = []
-
-                            // Decode the JSON as a dictionary with timestamps as keys
-                            let responseDict = try decoder.decode(YahooFinanceResponse.self, from: data)
-                            
-                            // Declare regularMarketPrice here
-                            let regularMarketPrice = responseDict.meta.regularMarketPrice
-                            
-                            //Take only last 30 timestamps
-                            let timestampItems = Array(responseDict.body.keys.sorted())
-                            let last3Timestamps = Array(timestampItems.suffix(60))
-                            
-                            // Now, financeResponse.body is a dictionary where the key is a string (UNIX timestamp)
-                            for timestampString in last3Timestamps {
-                                if let item = responseDict.body[timestampString], let timestamp = Int(timestampString) {
-                                    
-//                                    print(item)
-                                    
-                                    // Append StockHistory object
-                                    stockHistory.append(StockHistory(
-                                        timestamp: timestamp,
-                                        date: item.date,
-//                                        open: item.open,
-//                                        high: item.high,
-//                                        low: item.low,
-                                        close: item.close,
-                                        volume: item.volume
-                                    ))
-                                }
-                            }
-                            
-                            // You can update your UI here with the stock history
-                            DispatchQueue.main.async {
-                                // Update UI elements
-                                self.stocks.append(Stock(symbol: symbol, regularMarketPrice: regularMarketPrice, history: stockHistory))
-                            }
-                        } catch {
-                            print(error)
-                        }
-                    }
-                }.resume()
-
+////                if stock.symbol == "AAPL" || stock.symbol == "GM" {
+//                    print("Match Score for \(String(describing: stock.symbol)):", stock.matchScore)
+//                }
+ 
             }
+            
+            // Save the context after updating match scores
+            try viewContext.save()
+            
+        } catch {
+            print("Error fetching stocks: \(error)")
         }
     }
 }
 
-// View Model
+//Filter the top 20 stocks based on matchScore
+class StockFilterService {
+    func getTopStocks() async throws -> [DBStock] {
+        let viewContext = PersistenceController.shared.container.viewContext
+        
+        let fetchRequest: NSFetchRequest<DBStock> = DBStock.fetchRequest()
+        // Sort by matchScore in descending order to get the highest scores first
+        let sortDescriptor = NSSortDescriptor(key: #keyPath(DBStock.matchScore), ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Set the fetch limit to 20 to get only the top 20 stocks
+        fetchRequest.fetchLimit = 4
+        
+        do {
+            // Fetch the top 20 stocks
+            let topTwentyStocks = try viewContext.fetch(fetchRequest)
+            print("test Filtering")
+            print(topTwentyStocks)
+            return topTwentyStocks
+        } catch {
+            print("Error fetching stocks: \(error)")
+            throw error
+        }
+    }
+}
+
+//Fetch Price and Historical Prices for 20 selected stocks
+class StockAPIService {
+    // Fetch data from the API
+    func fetchDetailsForStocks(stocks: [DBStock]) async throws -> [DetailStock] {
+        print("Fetching API key...") // Print statement 1
+        guard let apiKey = fetchAPIKey() else {
+            throw NSError(domain: "StockAPIService", code: 0, userInfo: [NSLocalizedDescriptionKey: "API key is missing"])
+        }
+        print("API key fetched successfully.") // Print statement 2
+        
+        var details: [DetailStock] = []
+        
+        for stock in stocks {
+            print("Fetching details for stock: \(stock.symbol ?? "UNKNOWN")") // Print statement 3
+            let detailStock = try await fetchDetailForStock(stock: stock, apiKey: apiKey)
+            details.append(detailStock)
+            print("Details fetched for stock: \(stock.symbol ?? "UNKNOWN")") // Print statement 4
+        }
+        
+        return details
+    }
+    //Get the API Key from the Config File
+    private func fetchAPIKey() -> String? {
+        print("Accessing API key from Config file...") // Print statement 5
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {
+            print("Failed to access API key from Config file.") // Print statement 6
+            return nil
+        }
+        return dict["APIKey"] as? String
+    }
+    
+    //Get the price from a stock from the API
+    private func fetchDetailForStock(stock: DBStock, apiKey: String) async throws -> DetailStock {
+        print("Fetching details for stock: \(stock.symbol)")
+        let urlSymbol = stock.symbol ?? "AAPL"
+        let urlString = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=\(urlSymbol)&interval=1d&diffandsplits=false"
+        guard let url = URL(string: urlString) else {
+            throw NSError(domain: "StockAPIService", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue(apiKey, forHTTPHeaderField: "X-RapidAPI-Key")
+        request.setValue("yahoo-finance15.p.rapidapi.com", forHTTPHeaderField: "X-RapidAPI-Host")
+        
+        print("Requesting stock details from API for symbol: \(stock.symbol ?? "UNKNOWN")") // Print statement A
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            print("Received data from API for symbol: \(stock.symbol ?? "UNKNOWN")") // Print statement B
+            
+            do {
+                let responseDict = try JSONDecoder().decode(YahooFinanceResponse.self, from: data)
+                print("Decoded response successfully for symbol: \(stock.symbol ?? "UNKNOWN")") // Print statement C
+                
+                let regularMarketPrice = responseDict.meta.regularMarketPrice
+                var stockHistory: [StockHistory] = []
+                
+                let timestampItems = Array(responseDict.body.keys.sorted())
+                let lastTimestamps = Array(timestampItems.suffix(30)) // Adjust as necessary
+                
+                for timestampString in lastTimestamps {
+                    if let item = responseDict.body[timestampString], let timestamp = Int(timestampString) {
+                        stockHistory.append(StockHistory(
+                            timestamp: timestamp,
+                            date: item.date,
+                            close: item.close,
+                            volume: item.volume
+                        ))
+                    }
+                }
+                print("Processed historical data for symbol: \(stock.symbol ?? "UNKNOWN")") // Print statement D
+                
+                return DetailStock(
+                    symbol: stock.symbol ?? "DEFAULT",
+                    regularMarketPrice: regularMarketPrice,
+                    matchScore: stock.matchScore,
+                    peRatio: stock.peRatio,
+                    history: stockHistory
+                )
+            } catch {
+                print("Failed to decode response for symbol: \(stock.symbol ?? "UNKNOWN"). Error: \(error.localizedDescription)") // Print statement E
+                throw error
+            }
+        } catch {
+            print("Failed to fetch data from API for symbol: \(stock.symbol ?? "UNKNOWN"). Error: \(error.localizedDescription)") // Print statement F
+            throw error
+        }
+    }
+}
+
+
+
+//Bring together API Stocks and Stocks from CoreData
+class StockCoordinator: ObservableObject {
+    @Published var detailStocks: [DetailStock] = []
+    private let filterService = StockFilterService()
+    private let apiService = StockAPIService()
+
+    // This method can be called from a SwiftUI view directly to load stocks
+    func displayTopStocks() async {
+        do {
+            let topStocks = try await filterService.getTopStocks()
+            detailStocks = try await apiService.fetchDetailsForStocks(stocks: topStocks)
+        } catch {
+            // Handle any errors, perhaps by setting an error message in a @Published property
+        }
+    }
+}
+
+
+// View Model to get the Preferences of the User and calculate matchScore
 class MatchViewModel: ObservableObject {
     func updateMatchScores(peRatioPreference: Float) {
         let viewContext = PersistenceController.shared.container.viewContext
@@ -175,11 +242,13 @@ class MatchViewModel: ObservableObject {
 }
 
 
+
+
 class WatchlistViewModel: ObservableObject {
     static let shared = WatchlistViewModel()
-    @Published var watchlist: [Stock] = []
+    @Published var watchlist: [APIStock] = []
     
-    func addStock(stock: Stock) {
+    func addStock(stock: APIStock) {
         watchlist.append(stock)
     }
 }
