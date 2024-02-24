dffe98430a3ddca92d4fc6e42970817f26a40f80
Jakob Ossmann <Jakobossmann@gmail.com>
2024-02-24 22:35:12 +1000

    Onboarding button on slider change

--- ChartsSwiper/Views/PreferencesView.swift
+++ ChartsSwiper/Views/PreferencesView.swift
@@ -12,20 +12,23 @@ struct PreferencesView: View {
     
     @State private var currentQuestion = 1
     @State private var sliderValue: Double = 5
+    @State private var sliderMoved: Bool = false
+
 
     var body: some View {
-        VStack {
+        VStack(spacing: 80) {
             Text("What is important to you when investing?")
                 .font(.headline) // Changed to .headline for better visual hierarchy
                 .bold() // Ensures the question is bold
             
-            Spacer()
+            Spacer().frame(height: 20) // This Spacer pushes the button down
+
 
             switch currentQuestion {
             case 1:
-                preferenceQuestionView(imageName: "chart.bar.xaxis", title: "Revenue Growth", sliderValue: $sliderValue)
+                preferenceQuestionView(imageName: "chart.bar", title: "Revenue Growth", sliderValue: $sliderValue)
             case 2:
-                preferenceQuestionView(imageName: "chart.bar", title: "Hidden Value", sliderValue: $sliderValue)
+                preferenceQuestionView(imageName: "hand.coin", title: "Hidden Value", sliderValue: $sliderValue)
             case 3:
                 preferenceQuestionView(imageName: "arrow.up.forward", title: "Dividend Income", sliderValue: $sliderValue, isLast: true)
             default:
@@ -43,6 +46,10 @@ struct PreferencesView: View {
                 .bold() // Apply bold styling to the title
             Text("\(Int(sliderValue.wrappedValue))")
             Slider(value: sliderValue, in: 1...10, step: 1)
+            // Detect changes to the slider value
+                .onChange(of: sliderValue.wrappedValue) { _ in
+                    sliderMoved = true
+                }
             
             Spacer().frame(height: 16) // This Spacer pushes the button down
             
@@ -57,15 +64,20 @@ struct PreferencesView: View {
                 .cornerRadius(10)
                 .padding(.horizontal, 10)
             } else {
-                Button("Continue") {
-                    currentQuestion += 1
+                //only show if Slider was moved
+                if sliderMoved {
+                    Button("Continue") {
+                        currentQuestion += 1
+                        // Reset sliderMoved for the next question
+                        sliderMoved = false
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black) // Make the button black
+                    .cornerRadius(10)
+                    .padding(.horizontal, 10)
                 }
-                .foregroundColor(.white)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color.black) // Make the button black
-                .cornerRadius(10)
-                .padding(.horizontal, 10)
             }
         }
     }
